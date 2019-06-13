7820eca7aa95555d3ccc7241841db7afd22a87dc
Parsa <p_sadri_s@yahoo.com>
2019-06-13 12:56:16 +0430

    changed the hardcoded number in code to variable and added few comments

--- driverscoring/models.py
+++ driverscoring/models.py
@@ -6,21 +6,26 @@ from datetime import datetime, timedelta
 from math import radians, cos, sin, asin, sqrt
 from decimal import Decimal
 
+
+Number_of_decimal_places_for_coordination = 4
+Degree_of_area_around_request = 0.02
+Number_of_decimal_places_for_rating = 2
+
 # Create your models here.
 
 class Driver(models.Model):
-    latitude = models.DecimalField(max_digits=7, decimal_places=4)
-    longitude = models.DecimalField(max_digits=7, decimal_places=4)
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    latitude = models.DecimalField(max_digits=Number_of_decimal_places_for_coordination+3, decimal_places=Number_of_decimal_places_for_coordination)
+    longitude = models.DecimalField(max_digits=Number_of_decimal_places_for_coordination+3, decimal_places=Number_of_decimal_places_for_coordination)
+    rating = models.DecimalField(max_digits=Number_of_decimal_places_for_rating+1, decimal_places=Number_of_decimal_places_for_rating)
     last_trip_time = models.DateTimeField()
 
 
     @classmethod
     def create(cls, left, right, top, bottom):
-        rating = round(random.uniform(0, 5), 2)
-        longitude = round(random.uniform(left, right), 4)
-        latitude = round(random.uniform(bottom, top), 4)
-        # put last trip time some time between yesterday adn today
+        rating = round(random.uniform(0, 5), Number_of_decimal_places_for_rating)
+        longitude = round(random.uniform(left, right), Number_of_decimal_places_for_coordination)
+        latitude = round(random.uniform(bottom, top), Number_of_decimal_places_for_coordination)
+        # put last trip time some time between yesterday and today
         today = datetime.now()
         yesterday = datetime.now() - timedelta(days=1)
         stime = time.mktime(yesterday.timetuple())
@@ -37,7 +42,7 @@ class Driver(models.Model):
         score_dict = {}
         drivers = []
         for driver in Driver.objects.all():
-            if(abs(float(driver.latitude) - latitude) < 0.02 and abs(float(driver.longitude) - longitude) < 0.02):
+            if(abs(float(driver.latitude) - latitude) < Degree_of_area_around_request and abs(float(driver.longitude) - longitude) < Degree_of_area_around_request):
                 score = 0
                 # convert decimal degrees to radians
                 lon1, lat1, lon2, lat2 = map(radians, [float(driver.longitude), float(driver.latitude), longitude, latitude])
@@ -48,11 +53,15 @@ class Driver(models.Model):
                 c = 2 * asin(sqrt(a))
                 r = 6371
                 distance = c * r * 1000
+                # calculating score for driver
+                # score first is the distance in meters then is divided by rating
                 score = distance / float(driver.rating)
+                # then for every hour the driver hasn't had a request minus the score by 20
                 diff = datetime.now() - driver.last_trip_time
                 diff_hours = diff.seconds / 3600
                 score -= diff_hours*20
                 score_dict[score] = (driver.pk, distance)
+        # the lower the score comes first in list
         for i in sorted (score_dict) :
             drivers.append((Driver.objects.filter(pk=score_dict[i][0])[0].as_json(), score_dict[i][1]))
         return drivers
--- driverscoring/views.py
+++ driverscoring/views.py
@@ -7,30 +7,35 @@ import json
 
 from .models import Driver
 
+
+Number_of_decimal_places_for_coordination = 4
+Degree_of_area_around_request = 0.02
+
 # Create your views here.
 
 def index(request):
     context = {}
     return render(request, 'index.html', context)
-    
+
 
 @csrf_exempt
 @require_POST
 def matched(request):
     # check if POST objects has longitude and latitude
     if 'latitude' in request.POST and 'longitude' in request.POST:
-        latitude = round(float(request.POST['latitude']),4)
-        longitude = round(float(request.POST['longitude']),4)
+        latitude = round(float(request.POST['latitude']), Number_of_decimal_places_for_coordination)
+        longitude = round(float(request.POST['longitude']), Number_of_decimal_places_for_coordination)
         # delete all drivers from previwes run
         Driver.objects.all().delete()
         # area to look for drivers inside it
-        left = longitude - 0.02
-        right = longitude + 0.02
-        top = latitude + 0.02
-        bottom  = latitude - 0.02
+        left = longitude - Degree_of_area_around_request
+        right = longitude + Degree_of_area_around_request
+        top = latitude + Degree_of_area_around_request
+        bottom  = latitude - Degree_of_area_around_request
         # create 10 new random drivers
         for i in range(10):
             Driver.create(left, right, top, bottom)
+        # sort drivers for based on request
         drivers = Driver.match_driver(latitude, longitude)
 
         context = {'drivers': drivers }
